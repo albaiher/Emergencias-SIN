ccc18764c6bdcf4ba2a4a1632ca86b7f2d1dd6d0
jaime999 <martinezsanchezjaime@gmail.com>
2023-03-01 19:31:20 +0100

    Primer objetivo funciona

--- src/emergencies.py
+++ src/emergencies.py
@@ -6,42 +6,62 @@ state1 = pyhop.State('state1')
 
 state1.loc = {'ambulance': 'hospital', 'victim': 'university'}
 
-state1.coordinates = {'hospital': {'X': 10, 'Y': 10}, 'park': {'X': 14, 'Y': 14}, 'school': {'X': 5, 'Y': 5}, 'benimaclet': {'X': 8, 'Y': 8},
+state1.coordinates = {'hospital1': {'X': 10, 'Y': 10}, 'hospital2': {'X': 22, 'Y': 22},
+                       'park': {'X': 14, 'Y': 14}, 'school': {'X': 5, 'Y': 5}, 'benimaclet': {'X': 8, 'Y': 8},
                       'square': {'X': 0, 'Y': 0}, 'commercialCenter': {'X': 20, 'Y': 20}, 'university': {'X': 18, 'Y': 18}}
-state1.victims = {'victim': {'name': 'Andrés', 'age': 52, 'gravity': 5}}
-state1.hospitals = {'hospital': {'name': 'The War', 'loc': 'hospital'}}
-state1.ambulances = {'ambulance': {'name': 'Opel Corsa', 'maxGravity': 8}}
+state1.victims = {'victim': {'name': 'Andrés', 'age': 52, 'gravity': 4}}
+state1.hospitals = {'hospital1': {'name': 'La Paz', 'loc': 'hospital'}, 'hospital2': {'name': 'Universitario', 'loc': 'hospital2'}}
+state1.ambulances = {'ambulance': {'name': 'HK47', 'maxGravity': 2}}
 
 # Definición del GOAL
 goal1 = pyhop.Goal('goal1')
-goal1.loc = {'ambulance': 'hospital', 'victim': 'hospital'}
+goal1.loc = {'victim': 'hospital1'}
 
 # Funciones para la implementación del sistema
-def call_ambulance(state, victim, ambulance, dest):
-    if state.victims[victim].gravity <= state.ambulances[ambulance].maxGravity:
-        state.loc[ambulance] = dest
+def call_ambulance(state, dest):
+    if state.victims['victim']['gravity'] >= state.ambulances['ambulance']['maxGravity'] and state.loc['ambulance'] != dest:
+        state.loc['ambulance'] = dest
         return state
     else:
         return False
 
-def ride_ambulance(state, victim, ambulance, source, dest):
-    if state.loc[ambulance] == source and state.loc[victim] == source:
-        state.loc[ambulance] = dest
-        state.loc[victim] = dest
+
+def ride_ambulance(state, dest):
+    if state.loc['victim'] ==  'in_ambulance':
+        state.loc['ambulance'] = dest
         return state
     else:
         return False
     
-pyhop.declare_operators(call_ambulance, ride_ambulance)
+
+def load_ambulance(state):
+    if state.loc['ambulance'] ==  state.loc['victim']:
+        state.loc['victim'] = 'in_ambulance'
+        return state
+    else:
+        return False
+    
+
+def unload_ambulance(state, dest):
+    if state.loc['victim'] ==  'in_ambulance' and state.loc['ambulance'] == dest:
+        state.loc['victim'] = dest
+        return state
+    else:
+        return False
+    
+pyhop.declare_operators(call_ambulance, ride_ambulance, load_ambulance, unload_ambulance)
 print()
 pyhop.print_operators()
 
 # Definición de métodos para emergency
 
-def emergency_method(ambulance, victim, source, dest):
-    return [('call_ambulance', victim, ambulance, source), ('ride_ambulance', victim, ambulance, source, dest)]
+def deliver_victim(state, goal):
+    return [('call_ambulance', state.loc['victim']),
+            ('load_ambulance',),
+            ('ride_ambulance', goal.loc['victim']),
+            ('unload_ambulance', goal.loc['victim'])]
 
-pyhop.declare_methods('emergency_method', emergency_method)
+pyhop.declare_methods('deliver_victim', deliver_victim)
 print()
 pyhop.print_methods()
 
@@ -52,14 +72,14 @@ def emergency_recursive(state, goal):
             x = state.loc[who]
             y = goal.loc.pop(who)  # extraemos (se elimina) el goal del diccionario
             break
-        return [('emergency_method', who, x, y), ('travel', goal)]
+        return [('deliver_victim', who, x, y), ('emergency', goal)]
     return []  # caso base: si len(goal.loc) == 0
 
 # Indicamos cuál es la descomposición de "emergency"
 
-pyhop.declare_methods('emergency', emergency_recursive)
-print()
-pyhop.print_methods()
+# pyhop.declare_methods('emergency', emergency_recursive)
+# print()
+# pyhop.print_methods()
 
 
-pyhop.pyhop(state1, [('emergency', goal1)], verbose=1)
+pyhop.pyhop(state1, [('deliver_victim', goal1)], verbose=3)
